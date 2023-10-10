b58a597634cbdaf1bedf0e8b2a750bd6f04f9312
asan-31@mail.ru <asan-31@mail.ru>
2023-10-11 02:05:19 +0600

    feat: add function toReadable that converts given number, to readable str

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
-}
+  const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+  "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+  "seventeen", "eighteen", "nineteen"
+  ];
+
+  const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  if (number === 0) {
+    return "zero";
+  }
+
+  if (number < 20) {
+    return ones[number];
+  }
+
+  if (number < 100) {
+    const tenDigit = Math.floor(number / 10);
+    const oneDigit = number % 10;
+    return `${tens[tenDigit]}${oneDigit !== 0 ? ` ${ones[oneDigit]}` : ''}`
+  }
+
+  if (number < 1000) {
+    const hundredDigit = Math.floor(number / 100);
+    const remainingDigits = number % 100;
+    const remainingReadable = remainingDigits !== 0 ? ` ${toReadable(remainingDigits)}` : '';
+    return `${ones[hundredDigit]} hundred${remainingReadable}`;
+  }
+};
